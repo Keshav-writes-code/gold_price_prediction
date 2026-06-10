27365efef2c111d6f33af8b3817d1247072eb787
keshav-writes-code <dhimankeshav201@gmail.com>
2026-06-10 21:31:14 +0530

    refac: move cli to a seperate cli module

---
+++ src/cli/mod.rs
@@ -0,0 +1,52 @@
+use argh::{FromArgValue, FromArgs};
+
+#[derive(FromArgValue, Clone, Copy)]
+pub enum ModelArch {
+    LinearRegression,
+    Mlp,
+}
+
+#[derive(FromArgs)]
+/// Inpouts.
+struct Input {
+    /// start the taining loop and output a model artifact
+    #[argh(switch, short = 't')]
+    train: bool,
+
+    #[cfg(feature = "visualization")]
+    /// visualize the data
+    #[argh(switch, short = 'v')]
+    viz: bool,
+
+    /// pull_and_store_data
+    #[argh(switch, short = 'p')]
+    pull: bool,
+
+    /// serve the model
+    #[argh(switch, short = 's')]
+    serve: bool,
+
+    /// the model architecture to use for traning or infrence ('linear_regression', 'mlp')
+    #[argh(option, short = 'a', default = "ModelArch::LinearRegression")]
+    arch: ModelArch,
+}
+
+pub fn start_cli() {
+    crate::config::init();
+    let inputs: Input = argh::from_env();
+    if inputs.pull {
+        // logic to download data from cagle and store then in a sqlite database
+        crate::data::ingestion::hanlde_ingestion("raw_data.csv");
+    }
+    #[cfg(feature = "visualization")]
+    if inputs.viz {
+        // logic to visualize data
+        crate::data::visulization::init_visulization();
+    }
+    if inputs.train {
+        crate::models::training::train(&inputs.arch, "raw_data.csv");
+    }
+    if inputs.serve {
+        crate::serving::serve(&inputs.arch);
+    }
+}
